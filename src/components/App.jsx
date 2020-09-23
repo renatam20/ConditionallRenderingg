f221785fae8abcbd596a9700709216bac7a1430c
renatam20 <65694713+renatam20@users.noreply.github.com>
2020-09-22 21:50:00 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = true;
+
+const currentTime = new Date().getHours();
+
+function App() {
+  return (
+    <div className="container">
+      {
+        /* {isLoggedIn === true ? <h1>Hello</h1> : <Login />} */
+
+        // currentTime > 12 ? <h1>Why are you still working? </h1> : null
+
+        currentTime > 12 && <h1>Why are you still working? </h1>
+      }
+    </div>
+  );
+}
+
+export default App;
